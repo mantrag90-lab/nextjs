16e9d3d7ff1208602a21232a84c59825974194ae
mantra gupta <mantrag90@gmail.com>
2026-09-03 13:49:16 +0530

    Initial commit

---
+++ mantra/src/app/login/page.tsx
@@ -0,0 +1,8 @@
+export default function loginPage(){
+    return(
+
+        <div className="flex">
+           <h1 className="text-centre text-white ">login</h1> 
+        </div>
+    )
+}
---
+++ mantra/src/app/signup/page.tsx
@@ -0,0 +1,8 @@
+export default function SignupPage(){
+    return(
+
+        <div className="flex">
+           <h1 className="text-centre text-white ">signup</h1> 
+        </div>
+    )
+}
