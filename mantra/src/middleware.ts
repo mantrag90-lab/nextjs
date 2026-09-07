09daaaea9d6000a3d96b400bbcb64ae6c09d0855
mantra gupta <mantrag90@gmail.com>
2026-09-07 23:14:58 +0530

    middleware and me route

---
+++ mantra/src/middleware.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
+
+export function middleware(request: NextRequest) {
+const path = request.nextUrl.pathname
+
+const isPublicPath = path === '/Login' || path === '/signup'
+
+const token =request.cookies.get('token')?.value || ''  
+request.cookies.get('token')?.value||''
+
+if(isPublicPath && token){
+    return NextResponse.redirect(new URL('/', request.nextUrl))
+}
+}
+ 
+
+export const config = {
+  matcher: [
+    '/',
+    '/profile',
+    '/login',
+    'signup',
+
+  ]
+}
